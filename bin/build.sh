de09c249e88268b7007a89d4a96f830731323aae
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2020-06-05 22:50:04 +0300

    Automatically calculate donation amount and status

--- bin/build.sh
+++ bin/build.sh
@@ -21,8 +21,31 @@ add_links() {
     sed -E 's~ (http(s)://[^ ]+)~ <a href="\1">\1</a>~g'
 }
 
+update_donations() {
+    local feed
+    feed=$(cat <&0)
+
+    local donated
+    donated=$(echo "$feed" \
+        | awk '/[0-9]+ EUR$/ { sum+=$(NF-1);  } END { print sum  }')
+
+    local target
+    target=$(echo "$feed" \
+        | grep -E "\* target.+[0-9]+" \
+        | grep -Eo "[0-9]+"
+        )
+
+    local status
+    status=$(($donated-$target))
+
+    echo "$feed" \
+        | sed -E "s~( donated[^0-9]+)([0-9]+)~\1$donated~" \
+        | sed -E "s~( status[^-0-9]+)(-?[0-9]+)~\1$status~"
+}
+
+
 main() {
-    add_links | add_pr_links | add_user_links | build_html
+    add_links | add_pr_links | add_user_links | build_html | update_donations
 }
 
 main "$@"
