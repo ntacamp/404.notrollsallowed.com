d31c4f2a52c477317ea23980ccbc570858abb5a4
Simonas Kareiva <simonas@5grupe.lt>
2020-06-06 22:27:00 +0300

    Add markdown-style link generator

--- bin/build.sh
+++ bin/build.sh
@@ -17,12 +17,16 @@ add_user_links() {
     sed -E "s~@(\b.+\b)~<a href=\"https://github.com/\1\">@\1</a>~g"
 }
 
+add_markdown_links() {
+    sed -r -e 's/(.*)\[(([[:alnum:]]|[[:space:]])+)\]\((http([[:alnum:]]|[[:punct:]])+)\)(.*)/\1<a href="\4" target="_blank">\2<\/a>\6/g'
+}
+
 add_links() {
     sed -E 's~ (http(s)://[^ ]+)~ <a href="\1">\1</a>~g'
 }
 
 main() {
-    add_links | add_pr_links | add_user_links | build_html
+    add_links | add_pr_links | add_user_links | add_markdown_links | build_html
 }
 
 main "$@"
